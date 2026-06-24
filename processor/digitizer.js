287054befe83c1e295fb758e1c96304b2a8677f5
davyamadaos <dfmapping@gmail.com>
2026-06-24 18:29:55 +0100

    Update digitizer.js

--- processor/digitizer.js
+++ processor/digitizer.js
@@ -4,21 +4,16 @@ import sharp from "sharp";
 const URL =
     "https://epawebapp.epa.ie/hydronet/output/internet/stations/CAS/33008/S/extralarge_3m_extralarge.png";
 
-const LEFT = 65;
-const RIGHT = 650;
-
 const TOP = 24;
 const BOTTOM = 435;
 
 export async function extractLatest() {
 
-    const response =
-        await axios.get(URL, {
-            responseType: "arraybuffer"
-        });
+    const response = await axios.get(URL, {
+        responseType: "arraybuffer"
+    });
 
-    const image =
-        sharp(response.data);
+    const image = sharp(response.data);
 
     const { data, info } =
         await image
@@ -29,56 +24,41 @@ export async function extractLatest() {
 
     const width = info.width;
 
-    for (
-        let x = RIGHT;
-        x >= 560;
-        x--
-    ) {
+    for (let x = 650; x >= 560; x--) {
 
         const ys = [];
 
-        for (
-            let y = TOP;
-            y <= BOTTOM;
-            y++
-        ) {
+        for (let y = TOP; y <= BOTTOM; y++) {
 
-            const i =
-                (y * width + x) * 3;
+            const i = (y * width + x) * 3;
 
             const r = data[i];
             const g = data[i + 1];
             const b = data[i + 2];
 
-            if (
-                b > 150 &&
-                b > r + 40 &&
-                b > g + 40
-            ) {
+            const isBlue =
+                b > 120 &&
+                b > r + 20 &&
+                b > g + 20;
+
+            if (isBlue) {
                 ys.push(y);
             }
         }
 
-        // Ignore tiny groups.
+        // Filled area should be substantial.
 
-        if (ys.length >= 8) {
-
-            ys.sort((a, b) => a - b);
-
-            const y =
-                ys[Math.floor(
-                    ys.length / 2
-                )];
+        if (ys.length >= 20) {
 
             return {
                 x,
-                y,
+                y: ys[0],
                 count: ys.length
             };
         }
     }
 
     throw new Error(
-        "No river trace found"
+        "River trace not found"
     );
 }
