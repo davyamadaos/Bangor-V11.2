3a0b0b4e2c336ab493cfd2a864d490776ccd94a3
davyamadaos <dfmapping@gmail.com>
2026-06-24 07:28:26 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,30 +1,15 @@
 export const CONFIG = {
-
     station: "33008",
-
-    stationName: "Bangor River",
-
-    refreshMinutes: 15,
 
     epaImage:
         "https://epawebapp.epa.ie/hydronet/output/internet/stations/CAS/33008/S/extralarge_3m_extralarge.png",
 
-    forecastHours: [1, 3, 6],
-
     gaugeMultiplier: 16.921,
-
     gaugeOffset: 1675.7,
 
-    risingThreshold: 1,
-
-    fallingThreshold: -1
-
+    refreshMinutes: 15
 };
 
-export function gauge(level) {
-
-    return (
-        CONFIG.gaugeMultiplier * level
-        - CONFIG.gaugeOffset
-    );
+export function toGauge(level) {
+    return CONFIG.gaugeMultiplier * level - CONFIG.gaugeOffset;
 }
