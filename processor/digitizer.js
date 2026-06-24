3565aa49f7d89762e379d06f5ee858f75180e44b
davyamadaos <dfmapping@gmail.com>
2026-06-24 22:22:20 +0100

    Update digitizer.js

--- processor/digitizer.js
+++ processor/digitizer.js
@@ -6,11 +6,13 @@ const URL =
 
 export async function extractLatest() {
 
-    const response = await axios.get(URL, {
-        responseType: "arraybuffer"
-    });
+    const response =
+        await axios.get(URL, {
+            responseType: "arraybuffer"
+        });
 
-    const image = sharp(response.data);
+    const image =
+        sharp(response.data);
 
     const { data, info } =
         await image
@@ -21,13 +23,14 @@ export async function extractLatest() {
 
     const width = info.width;
 
-    for (let x = 665; x >= 600; x--) {
+    for (let x = 665; x >= 620; x--) {
 
-        let highest = null;
+        const ys = [];
 
-        for (let y = 350; y <= 435; y++) {
+        for (let y = 340; y <= 435; y++) {
 
-            const i = (y * width + x) * 3;
+            const i =
+                (y * width + x) * 3;
 
             const r = data[i];
             const g = data[i + 1];
@@ -37,31 +40,51 @@ export async function extractLatest() {
                 b > r + 20 &&
                 b > g + 20
             ) {
-                highest = y;
-                break;
+                ys.push(y);
             }
         }
 
-        if (
-            highest !== null &&
-            highest < 410
-        ) {
+        if (ys.length < 3)
+            continue;
 
-            console.log(
-                "Column:",
-                x
-            );
+        const groups = [];
 
-            console.log(
-                "River Y:",
-                highest
-            );
+        let current = [ys[0]];
 
-            return {
-                x,
-                y: highest,
-                count: 1
-            };
+        for (let i = 1; i < ys.length; i++) {
+
+            if (ys[i] <= ys[i - 1] + 8) {
+                current.push(ys[i]);
+            } else {
+                groups.push(current);
+                current = [ys[i]];
+            }
+        }
+
+        groups.push(current);
+
+        // Use the lowest group with at least 3 pixels.
+
+        for (let i = groups.length - 1; i >= 0; i--) {
+
+            const g = groups[i];
+
+            if (g.length >= 3) {
+
+                console.log("Column:", x);
+                console.log(
+                    "River:",
+                    g[0],
+                    "-",
+                    g[g.length - 1]
+                );
+
+                return {
+                    x,
+                    y: g[0],
+                    count: g.length
+                };
+            }
         }
     }
 
