78c5779b2c332bce43b326b6e469fd48574b8380
davyamadaos <dfmapping@gmail.com>
2026-06-24 18:53:24 +0100

    Update digitizer.js

--- processor/digitizer.js
+++ processor/digitizer.js
@@ -4,16 +4,18 @@ import sharp from "sharp";
 const URL =
     "https://epawebapp.epa.ie/hydronet/output/internet/stations/CAS/33008/S/extralarge_3m_extralarge.png";
 
-const TOP = 24;
+const TOP = 250;
 const BOTTOM = 435;
 
 export async function extractLatest() {
 
-    const response = await axios.get(URL, {
-        responseType: "arraybuffer"
-    });
+    const response =
+        await axios.get(URL, {
+            responseType: "arraybuffer"
+        });
 
-    const image = sharp(response.data);
+    const image =
+        sharp(response.data);
 
     const { data, info } =
         await image
@@ -24,13 +26,14 @@ export async function extractLatest() {
 
     const width = info.width;
 
-    for (let x = 650; x >= 560; x--) {
+    for (let x = 650; x >= 580; x--) {
 
         const ys = [];
 
         for (let y = TOP; y <= BOTTOM; y++) {
 
-            const i = (y * width + x) * 3;
+            const i =
+                (y * width + x) * 3;
 
             const r = data[i];
             const g = data[i + 1];
@@ -46,16 +49,38 @@ export async function extractLatest() {
             }
         }
 
-        // Filled area should be substantial.
+        if (ys.length < 5)
+            continue;
 
-        if (ys.length >= 20) {
+        const groups = [];
 
-            return {
-                x,
-                y: ys[0],
-                count: ys.length
-            };
+        let current = [ys[0]];
+
+        for (let i = 1; i < ys.length; i++) {
+
+            if (ys[i] <= ys[i - 1] + 2) {
+                current.push(ys[i]);
+            } else {
+                groups.push(current);
+                current = [ys[i]];
+            }
         }
+
+        groups.push(current);
+
+        // Lowest group on chart.
+        const river =
+            groups[groups.length - 1];
+
+        // Ignore tiny groups.
+        if (river.length < 4)
+            continue;
+
+        return {
+            x,
+            y: river[0],
+            count: river.length
+        };
     }
 
     throw new Error(
