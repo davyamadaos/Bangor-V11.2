4c0c0a855ea9fd78808e640e1cc8370d772e2590
davyamadaos <dfmapping@gmail.com>
2026-06-25 07:04:24 +0100

    Create tide.js

---
+++ js/tide.js
@@ -0,0 +1,39 @@
+<section class="card">
+
+    <div class="label">
+        Tide State
+    </div>
+
+    <div id="tideState" class="big">
+        --
+    </div>
+
+    <table class="table">
+
+        <tr>
+            <td>Previous Low</td>
+            <td id="prevLow"></td>
+        </tr>
+
+        <tr>
+            <td>Previous High</td>
+            <td id="prevHigh"></td>
+        </tr>
+
+        <tr>
+            <td>Next High</td>
+            <td id="nextHigh"></td>
+        </tr>
+
+        <tr>
+            <td>Next Low</td>
+            <td id="nextLow"></td>
+        </tr>
+
+    </table>
+
+</section>
+
+<section class="card">
+    <canvas id="tideChart"></canvas>
+</section>
